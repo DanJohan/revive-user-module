5f06fa54997e0affcb07ed386a71e0bc0e2a56ce
komalsmartserve <komal.smartserve@gmail.com>
2018-11-16 16:34:53 +0530

    add slider

--- application/views/layouts/home/navbar.php
+++ application/views/layouts/home/navbar.php
@@ -62,7 +62,7 @@
 <div class="container align-self-center">
     <div class="row align-items-center">
         <div class="col-sm-7">
-            <h3 class="ts-font-color__black ts-opacity__50">Revive your ride</h3>
+            <h3>Revive your ride</h3>
             <h1>The Ultimate Body Shop Experience!</h1>
             <a href="<?php echo base_url();?>service/select_service" class="btn btn-primary btn-lg ts-scroll" data-bg-color="#47143d">Book Now</a>
             <a href="<?php echo base_url();?>user/login" class="btn btn-primary btn-lg ts-scroll" data-bg-color="#47143d">Service Status</a>
@@ -85,15 +85,15 @@
 </div>
 <!--end container-->
 <div id="ts-dynamic-waves" class="ts-background">
-    <svg class="ts-svg ts-z-index__1 ts-flip-x" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <!-- s<svg class="ts-svg ts-z-index__1 ts-flip-x" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <defs></defs>
         <path class="ts-dynamic-wave" id="wave-1" d="" data-wave-color="#fff" data-wave-height=".3" data-wave-bones="4" data-wave-speed="0.5"/>
     </svg>
     <svg class="ts-svg ts-z-index__1" width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
     <defs></defs>
         <path class="ts-dynamic-wave" id="wave-2" d="" data-wave-color="#fff" data-wave-height=".3" data-wave-bones="6" data-wave-speed="0.3"/>
-    </svg>
-    <div class="ts-background-image ts-parallax-element" data-bg-image="<?php echo base_url();?>assets/img/bg-blurred-run.jpg">
+    </svg> -->
+    <!-- <div class="ts-background-image ts-parallax-element" data-bg-image="<?php echo base_url();?>assets/img/bg-blurred-run.jpg"> -->
     </div>
 </div>
 </header>
